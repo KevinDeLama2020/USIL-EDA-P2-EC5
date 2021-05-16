de7bc3042009f1e744de06ea4f84a2bf757d5b8d
Kevin De Lama <74796011+KevinDeLama2020@users.noreply.github.com>
2021-05-16 18:46:39 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+#include "bst.h"
+#include "type.h"
 
+using namespace std;
 int main()
 {
-  std::cout << "Hello World!\n";
+  CBinaryTree<T1>(obj);
+
+  T1 x=20, y=18, z=15,k=19, a=30, b=25, c=39;
+
+  obj.insert(x);
+  //cout<<"prueba 1"<<endl;
+  obj.insert(y);
+  //cout<<"prueba 2"<<endl;
+  obj.insert(z);
+  //cout<<"prueba 3"<<endl;
+  obj.insert(k);
+  //cout<<"prueba 4"<<endl;
+  obj.insert(a);
+  //cout<<"prueba 5"<<endl;
+  obj.insert(b);
+  //cout<<"prueba 6"<<endl;
+  obj.insert(c);
+  //cout<<"prueba 7"<<endl;
+
+  obj.printbase();
+
 }
